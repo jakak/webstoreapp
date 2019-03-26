9ff7b84742c63d716f4a77c1869b4230e4817141
James John James <james@aboutsic.com>
2019-03-26 18:58:39 +0100

    project setup

---
+++ packages/Webkul/Shop/src/Resources/assets/js/components/customFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>
+        &copy; {{getDate()}} Kling Bake Shop | <a href="https://aboutsic.com/webmaster" target="_blank">Webmaster</a>
+    </p>
+</template>
+
+<script>
+export default {
+    name: 'customFooter',
+    methods: {
+        getDate () {
+            return new Date().getFullYear();
+        }
+    }
+}
+</script>
