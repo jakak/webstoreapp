dabfb67b6b785d72ef5b266c168e41f6867e13c3
James John James <jamesjay4199@gmail.com>
2019-04-13 14:59:26 +0100

    bug fixes

--- packages/Webkul/Shop/src/Resources/views/emails/customer/verification-email.blade.php
+++ packages/Webkul/Shop/src/Resources/views/emails/customer/verification-email.blade.php
@@ -10,8 +10,6 @@
         <div  style="font-size:16px; color:#242424; font-weight:600; margin-top: 60px; margin-bottom: 15px">
             Hello,
         </div>
-        <p>Hello,</p>
-        <p></p>
         <p>
             Thank you for choosing <strong>KlingBakeShop!</strong> 
             You are just one click away from completing your account registration.
