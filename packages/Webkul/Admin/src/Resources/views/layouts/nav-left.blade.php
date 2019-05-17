f7d2fd7dc811b0e92b02b7909d01af0d535db37e
James John James <jamesjay4199@gmail.com>
2019-05-17 17:38:07 +0100

    nav icons

--- packages/Webkul/Admin/src/Resources/views/layouts/nav-left.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/nav-left.blade.php
@@ -3,7 +3,7 @@
         @foreach ($menu->items as $menuItem)
             <li class="menu-item {{ $menu->getActive($menuItem) }}">
                 <a href="{{ count($menuItem['children']) ? current($menuItem['children'])['url'] : $menuItem['url'] }}">
-                    <span class="icon {{ $menuItem['icon-class'] }}">
+                    <span class="icon {{ $menuItem['icon-class'] }}" style="background-image:url({{ asset('images/' . $menuItem['key']) . '.svg' }})">
                     </span>
                     {{ trans($menuItem['name']) }}
                 </a>
