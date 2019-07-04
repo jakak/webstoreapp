edfbed9aa208bdc0ca8cba999ab271b9927c0183
James John James <jamesjay4199@gmail.com>
2019-07-04 19:27:40 +0100

    image cache issue and storemanager redirect

--- packages/Webkul/Admin/src/Http/routes.php
+++ packages/Webkul/Admin/src/Http/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::group(['middleware' => ['web']], function () {
-    Route::prefix('admin')->group(function () {
+    Route::prefix('storemanager')->group(function () {
 
         Route::get('/', 'Webkul\Admin\Http\Controllers\Controller@redirectToLogin');
 
