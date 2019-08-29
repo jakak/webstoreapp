2494347f15da6d55c6d39d917e709dac97c01f59
James James <jamesjay4199@gmail.com>
2019-08-29 22:16:27 +0100

    remove install completely

--- public/index.php
+++ public/index.php
@@ -1,27 +1,19 @@
 <?php
 
-$install = require __DIR__.'/installer/install.php';
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
 
-if (!is_null($install)) {
+require __DIR__.'/../vendor/autoload.php';
 
-    header("Location: $install");
-
-} else {
-
-    /*
-    |--------------------------------------------------------------------------
-    | Register The Auto Loader
-    |--------------------------------------------------------------------------
-    |
-    | Composer provides a convenient, automatically generated class loader for
-    | our application. We just need to utilize it! We'll simply require it
-    | into the script here so that we don't have to worry about manual
-    | loading any of our classes later on. It feels great to relax.
-    |
-    */
-
-    require __DIR__.'/../vendor/autoload.php';
-}
 
 
 /**
