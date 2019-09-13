42d69e999bc121b29c09412a6b987769c7076d77
James James <jamesjay4199@gmail.com>
2019-09-13 15:12:26 +0100

    remove unused event listener

--- app/MailSetting.php
+++ app/MailSetting.php
@@ -2,7 +2,6 @@
 
 namespace App;
 
-use App\Events\MailSettingSaved;
 use Illuminate\Database\Eloquent\Model;
 use Watson\Rememberable\Rememberable;
 
@@ -11,9 +10,6 @@ class MailSetting extends Model
     use Rememberable;
 
     protected $fillable = ['logo', 'host', 'port', 'encryption', 'username', 'password'];
-    protected $dispatchesEvents = [
-        'saving' => MailSettingSaved::class
-    ];
 
     public $rememberCacheTag = 'mail_setting_queries';
 //    public $rememberFor = 60 * 60;
