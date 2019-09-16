0cff6548a6651d4b28ae77e6e1b53ecba8ad7cf4
James James <jamesjay4199@gmail.com>
2019-09-16 22:45:50 +0100

    remove unnecessary migration

--- database/migrations/2019_09_17_010333_remove_locale_feature.php
+++ database/migrations/2019_09_17_010333_remove_locale_feature.php
@@ -13,20 +13,6 @@ class RemoveLocaleFeature extends Migration
      */
     public function up()
     {
-        Schema::table('channels', function(Blueprint $table) {
-            try {
-                $table->dropForeign('default_locale_id');
-                $table->dropColumn('default_locale_id');
-            } catch (Exception $e) {
-                echo 'column already dropped';
-            }
-        });
-        Schema::table('product_attribute_values', function(Blueprint $table) {
-            $table->dropForeign('locale');
-            $table->dropColumn('locale');
-        });
-        Schema::dropIfExists('channel_locales');
-//        Schema::dropIfExists('locales');
 
     }
 
@@ -37,24 +23,6 @@ class RemoveLocaleFeature extends Migration
      */
     public function down()
     {
-        /*Schema::create('locales', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('code')->unique();
-            $table->string('name');
-            $table->timestamps();
-        });*/
 
-        Schema::create('channel_locales', function (Blueprint $table) {
-            $table->integer('channel_id')->unsigned();
-            $table->integer('locale_id')->unsigned();
-            $table->primary(['channel_id', 'locale_id']);
-            $table->foreign('channel_id')->references('id')->on('channels')->onDelete('cascade');
-            $table->foreign('locale_id')->references('id')->on('locales')->onDelete('cascade');
-        });
-
-        Schema::table('channels', function(Blueprint $table) {
-            $table->integer('default_locale_id')->nullable()->unsigned();
-            $table->foreign('default_locale_id')->references('id')->on('locales')->onDelete('cascade');
-        });
     }
 }
