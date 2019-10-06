3fdf1779b8c10197f39795980f48a1323bc6af6e
James James <jamesjay4199@gmail.com>
2019-10-06 21:38:15 +0100

    hot fix to be reverted

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Webkul\Admin\Database\Seeders\DatabaseSeeder as BagistoDatabaseSeeder;
+use Webkul\Attribute\Database\Seeders\AttributeGroupTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(BagistoDatabaseSeeder::class);
+//        $this->call(BagistoDatabaseSeeder::class);
+        $this->call(AttributeGroupTableSeeder::class);
     }
 }
