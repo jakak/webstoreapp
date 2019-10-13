4b0eb133d0364e05eebf24d2976cfa17e9e2f3c4
uyoobonga <uyoobonga@gmail.com>
2019-10-13 20:30:51 +0100

    Fixed issue on the store name

--- packages/Webkul/Admin/src/Resources/views/layouts/nav-top.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/nav-top.blade.php
@@ -15,10 +15,6 @@
             </div>
 
             <div class="profile-info">
-                <a href="{{ route('shop.home.index') }}" target="_blank" title="{{ trans('admin::app.layouts.storefront') }}"><i class="fas fa-external-link-alt"></i></a>
-            </div>
-
-            <div class="profile-info">
                 <div class="dropdown-toggle">
                     <div style="display: inline-block; vertical-align: middle;">
                         <i class="icon avatar active"></i>
@@ -61,6 +57,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="ld-scale">
                 <div  class="profile-info">
                     <div class="dropdown-toggle">
@@ -81,8 +78,10 @@
                 </div>
             </div>
 
-            <div  class="profile-info">
-                <a class="store-name" style="color: #79C142" href="{{ route('shop.home.index') }}" target="_blank" title="{{ trans('admin::app.layouts.storefront') }}">{{ core()->getCurrentChannel()->name }} <i class="fas fa-external-link-alt"></i></a>
+            <div class="md-scale">
+                <div  class="profile-info">
+                    <a class="store-name" style="color: #79C142" href="{{ route('shop.home.index') }}" target="_blank" title="{{ trans('admin::app.layouts.storefront') }}">{{ core()->getCurrentChannel()->name }} <i class="fas fa-external-link-alt"></i></a>
+                </div>
             </div>
 
         </div>
