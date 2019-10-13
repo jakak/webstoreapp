1dda0f5e283c841178233fdb7bdd00c6931da74e
uyoobonga <uyoobonga@gmail.com>
2019-10-13 21:44:37 +0100

    changes has been fixed

--- packages/Webkul/Shop/src/Resources/views/checkout/onepage/shipping.blade.php
+++ packages/Webkul/Shop/src/Resources/views/checkout/onepage/shipping.blade.php
@@ -9,10 +9,11 @@
             <div class="control-group" :class="[errors.has('shipping-form.shipping_method') ? 'has-error' : '']">
                 <select type="text" placeholder="Select Store Pickup or Delivery Location" v-model="selected_shipping_method" v-validate="'required'" class="control" id="shipping" name="shipping_method" @change="methodSelected()">
                     @foreach ($shippingRateGroups as $rateGroup)
-                        @foreach ($rateGroup['rates'] as $rate)
-                            <option value="{{ $rate->method }}"> {{ $rate->method_title }}</option>
-                        @endforeach
-                        <hr style="border: 2px solid #C7C7C7; width: 70%;">
+                        <optgroup label="Pick up your Items In-store">
+                            @foreach ($rateGroup['rates'] as $rate)
+                                <option value="{{ $rate->method }}"> {{ $rate->method_title }}</option>
+                            @endforeach
+                        </optgroup>
                     @endforeach
                     <optgroup label="Select Delivery Location">
                         @foreach ($location as $loc)
@@ -21,19 +22,6 @@
                     </optgroup>
                 </select>
                 <label for="shipping" id="shippingLabel"> {!! core()->getConfigData('sales.carriers.free.description') !!}</label>
-                {{-- @foreach ($shippingRateGroups as $rateGroup)
-                    <h4 for="">{{ $rateGroup['carrier_title'] }}</h4>
-
-                    @foreach ($rateGroup['rates'] as $rate)
-                        <span class="radio" >
-                            <input v-validate="'required'" type="radio" id="{{ $rate->method }}" name="shipping_method" data-vv-as="&quot;{{ __('shop::app.checkout.onepage.delivery-method') }}&quot;" value="{{ $rate->method }}" v-model="selected_shipping_method" @change="methodSelected()">
-                            <label class="radio-view" for="{{ $rate->method }}"></label>
-                            {{ $rate->method_title }}
-                            <b>{{ core()->currency($rate->base_price) }}</b>
-                        </span>
-                    @endforeach
-
-                @endforeach --}}
 
                 <span class="control-error" v-if="errors.has('shipping-form.shipping_method')">
                     @{{ errors.first('shipping-form.shipping_method') }}
