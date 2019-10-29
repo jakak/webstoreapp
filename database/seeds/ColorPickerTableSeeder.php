41b20dfb847a7b7fc164c8b31a0c468ad65562ce
uyoobonga <uyoobonga@gmail.com>
2019-10-29 15:46:26 +0100

    fixed default color

--- database/seeds/ColorPickerTableSeeder.php
+++ database/seeds/ColorPickerTableSeeder.php
@@ -14,13 +14,13 @@ class ColorPickerTableSeeder extends Seeder
     {
         ColorPicker::updateOrCreate([
             'top_menu_bg' => 'ffffff',
-            'top_menu_text' => '000000',
-            'cart_button_bg' => 'b670af',
+            'top_menu_text' => '4f5b64',
+            'cart_button_bg' => '79c142',
             'cart_button_text' => 'ffffff',
             'footer_bg' => 'f2f2f2',
             'footer_title' => 'a5a5a5',
-            'footer_text' => '242424',
-            'footer_button' => '242424'
+            'footer_text' => '4f5b64',
+            'footer_button' => '4f5b64'
         ]);
     }
 }
