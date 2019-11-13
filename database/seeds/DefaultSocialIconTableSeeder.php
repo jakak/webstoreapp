00a0abb92a50344c07ab5e866a38d602aceca540
uyoobonga <uyoobonga@gmail.com>
2019-11-13 19:23:11 +0100

    flat icons has been handle and also some bugs

--- database/seeds/DefaultSocialIconTableSeeder.php
+++ database/seeds/DefaultSocialIconTableSeeder.php
@@ -16,21 +16,21 @@ class DefaultSocialIconTableSeeder extends Seeder
 
         SocialIcon::create([
             'name' => 'Instagram',
-            'url' => 'https://',
+            'url' => 'https://instagram.com/',
             'icon' => '<span><i class="fab fa-instagram fa-1x" style="font-size: 1.5em; color: rgb(79, 91, 100); margin-right: 15px;"></i></span>',
             'icon_name' => 'default_icon_theme'
         ]);
 
         SocialIcon::create([
             'name' => 'Twitter',
-            'url' => 'https://',
+            'url' => 'https://twitter.com/',
             'icon' => '<span><i style="font-size: 1.5em; color: rgb(79, 91, 100); margin-right: 15px;" class="fab fa-twitter fa-1x"></i></span>',
             'icon_name' => 'default_icon_theme'
         ]);
 
         SocialIcon::create([
             'name' => 'Facebook',
-            'url' => 'https://',
+            'url' => 'https://facebook.com/',
             'icon' => '<span><i class="fab fa-facebook-f fa-1x" style="font-size: 1.5em; color: rgb(79, 91, 100); margin-right: 15px;"></i></span>',
             'icon_name' => 'default_icon_theme'
         ]);
@@ -38,14 +38,14 @@ class DefaultSocialIconTableSeeder extends Seeder
 
         SocialIcon::create([
             'name' => 'Youtube',
-            'url' => 'https://',
+            'url' => 'https://youtube.com/',
             'icon' => '<span><i class="fab fa-youtube fa-1x" style="font-size: 1.5em; color: rgb(79, 91, 100); margin-right: 15px;"></i></span>',
             'icon_name' => 'default_icon_theme'
         ]);
 
         SocialIcon::create([
             'name' => 'Whatsapp',
-            'url' => 'https://',
+            'url' => 'https://wa.me/',
             'icon' => '<span><i style="font-size: 1.5em; color: rgb(79, 91, 100); margin-right: 15px;" class="fab fa-whatsapp"></i></span>',
             'icon_name' => 'default_icon_theme'
         ]);
--- packages/Webkul/Shop/src/Resources/views/layouts/footer/footer_content.blade.php
+++ packages/Webkul/Shop/src/Resources/views/layouts/footer/footer_content.blade.php
@@ -17,11 +17,11 @@
             <form action="{{ route('admin.configuration.footer.pages.create') }}" enctype="multipart/form-data" method="POST">
                 <div class="form-container">
                     @csrf
-                    <accordian :title="'Page Links'" :active="true">
+                    <accordian :title="'Page Links'" :active="false">
                             @include('shop::layouts.footer.page_link')
                     </accordian>
 
-                    <accordian :title="'Social Links'" :active="false">
+                    <accordian :title="'Social Links'" :active="true">
                             @include('shop::layouts.footer.social_link')
                     </accordian>
 
